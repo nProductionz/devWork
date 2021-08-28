3b4fddbfa423a172e449004dec449e36d6d8614b
nProductionz <nProductionz@users.noreply.github.com>
2021-08-28 17:57:58 +0200

    artist controller, created log.txt

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,59 +1,45 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: %i[ show edit update destroy ]
 
-  # GET /artists or /artists.json
   def index
     @artists = Artist.all
   end
 
-  # GET /artists/1 or /artists/1.json
   def show
+    @artist = Artist.find(params[:id])
   end
 
-  # GET /artists/new
   def new
     @artist = Artist.new
   end
 
-  # GET /artists/1/edit
   def edit
+    @artist = Artist.find(params[:id])
   end
 
-  # POST /artists or /artists.json
   def create
     @artist = Artist.new(artist_params)
-
-    respond_to do |format|
-      if @artist.save
-        format.html { redirect_to @artist, notice: "Artist was successfully created." }
-        format.json { render :show, status: :created, location: @artist }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
+    if @artist.save
+      redirect_to @artist
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /artists/1 or /artists/1.json
   def update
-    respond_to do |format|
-      if @artist.update(artist_params)
-        format.html { redirect_to @artist, notice: "Artist was successfully updated." }
-        format.json { render :show, status: :ok, location: @artist }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @artist.errors, status: :unprocessable_entity }
-      end
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to @artist
+    else
+      render :edit
     end
   end
 
-  # DELETE /artists/1 or /artists/1.json
   def destroy
+    @artist = Artist.find(params[:id])
     @artist.destroy
-    respond_to do |format|
-      format.html { redirect_to artists_url, notice: "Artist was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
+    redirect_to root_path
   end
 
   private
